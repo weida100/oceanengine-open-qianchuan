e47b359037757daa9e2ded399875105c4c747e49
Sgenmi <sgenmi@gmail.com>
2023-11-05 23:28:45 +0800

    init

---
+++ src/Application.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Weida\OceanengineOpenQianchuan;
+
+use Weida\OceanengineCore\AbstractApplication;
+
+/**
+ * Author: Weida
+ * Date: 2023/11/5 12:41
+ * Email: sgenmi@gmail.com
+ */
+class Application extends AbstractApplication
+{
+
+
+
+}
