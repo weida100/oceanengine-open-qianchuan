356ee7e671c68f4dc5a246c9ca69bcd98b6a7469
Weida <sgenmi@gmail.com>
2023-11-06 23:55:18 +0800

    .

--- src/Application.php
+++ src/Application.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace Weida\OceanengineOpenQianchuan;
 
 use Weida\OceanengineCore\AbstractApplication;
+use Weida\OceanengineCore\Contract\WithAccessTokenClientInterface;
+use Weida\OceanengineCore\WithAccessTokenClient;
 
 /**
  * Author: Weida
@@ -12,7 +14,46 @@ use Weida\OceanengineCore\AbstractApplication;
  */
 class Application extends AbstractApplication
 {
+    protected string $cacheNamespace="weida:oceanengine:qianchuan";
+    private WithAccessTokenClientInterface $client;
 
+    /**
+     * @param int $uid
+     * @param string $refreshToken
+     * @return $this
+     * @author Weida
+     */
+    public function withRefreshToken(int $uid,string $refreshToken):static{
+        $this->client = new WithAccessTokenClient(
+            $this->getHttpClient(),
+            $this->getAccessToken($uid,$refreshToken)
+        );
+        return $this;
+    }
+
+    /**
+     * @param int $uid
+     * @param string $accessToken
+     * @return $this
+     * @author Weida
+     */
+    public function withAccessToken(int $uid,string $accessToken):static {
+        $_at =  $this->getAccessToken($uid,'');
+        $_at->setToken($accessToken);
+        $this->client = new WithAccessTokenClient(
+            $this->getHttpClient(),
+            $_at
+        );
+        return $this;
+    }
+
+    /**
+     * @return WithAccessTokenClientInterface
+     * @author Weida
+     */
+    public function getClient():WithAccessTokenClientInterface{
+        return $this->client;
+    }
 
 
 }
